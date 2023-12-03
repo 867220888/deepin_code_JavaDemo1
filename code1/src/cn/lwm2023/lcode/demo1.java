fd23d710a0458fd4f09cdf3a1c21fcb5c6cb1873
867220888 <32165900016@e.gzhu.edu.cn>
2023-12-03 14:05:58 +0800

    首次提交

--- code1/src/cn/lwm2023/lcode/demo1.java
+++ code1/src/cn/lwm2023/lcode/demo1.java
@@ -1,19 +1,12 @@
 package cn.lwm2023.lcode;
 
-import java.util.Scanner;
-
 public class demo1 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("请输入两个数字表示范围：");
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int sum = 0;
-        for (int i = a; i <= b; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                sum++;
-            }
+        int [] ages = new int[] {18, 19, 20};
+        String [] names = new String[] {"张三", "李四", "王五"};
+        double [] scores = new double[] {99.5, 98.5, 97.5};
+        for (int i = 0; i < ages.length; i++) {
+            System.out.println("姓名：" + names[i] + "，年龄：" + ages[i] + "，成绩：" + scores[i]);
         }
-        System.out.println("范围内能被3和5整除的数字有：" + sum + "个");
     }
 }
